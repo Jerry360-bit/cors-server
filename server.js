8b13b54fb51b724069d248fec44e2e850b7a8779
Jerry360-bit <gerben@roosenboom.nu>
2023-04-10 13:07:14 +0200

    https tryout

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const corsProxy = require('cors-anywhere');
 // Listen on a specific host via the HOST environment variable.
 const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable.
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'http://spots.portableprojects.nl'],
+    originWhitelist: ['localhost:3000', 'localhost:3000/dashboard/settings', 'spots.portableprojects.nl'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
