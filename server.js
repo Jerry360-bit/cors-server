9a3b60dd4386d6c0a83a8c031110f4a8aa266d50
Jerry360-bit <gerben@roosenboom.nu>
2023-06-08 16:37:03 +0200

    8-6-2023

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'https://spots.portableprojects.nl'],
+    originWhitelist: ['http://localhost:3000', 'http://localhost:3000/dashboard/settings', 'https://spots.portableprojects.nl', 'https://spot-forecast-production.up.railway.app'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
